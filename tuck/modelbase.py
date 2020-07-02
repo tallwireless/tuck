fbb944a6213cdd5ec6a5714907652f5f11520101
Charles Rumford <charlesr@tallwireless.com>
2020-07-02 16:17:36 -0400

    Adding some default columns

--- tuck/modelbase.py
+++ tuck/modelbase.py
@@ -1,13 +1,35 @@
 from tuck.db import db_session
+from sqlalchemy import DateTime, String, Column
+
+from flask import g
+
+import arrow
 
 
 class ModelBase(object):
+    createdBy = Column(String(50), nullable=False)
+    created = Column(DateTime, nullable=False)
+    updatedBy = Column(String(50), nullable=False)
+    updated = Column(DateTime, nullable=False)
+
     def __init__(self):
         pass
 
-    def save(self):
-        db_session.add(self)
-        db_session.commit()
+    def save(self, who=None):
+        if self._sa_instance_state.modified:
+            if who is None:
+                who = g.user.username
+            # The row is brand new, need to say who created it
+            if self.createdBy is None:
+                self.createdBy = who
+                self.created = arrow.utcnow().datetime
+
+            # Update who updated it and when
+            self.updatedBy = who
+            self.updated = arrow.utcnow().datetime
+
+            db_session.add(self)
+            db_session.commit()
 
     def delete(self):
         db_session.delete(self)
