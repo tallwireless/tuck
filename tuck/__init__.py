aae3f3599ccdc5205bf2656d32ee0c8a079b67c6
Charles Rumford <charlesr@tallwireless.com>
2020-07-02 17:13:42 -0400

    Working device views for getting device objects

--- tuck/__init__.py
+++ tuck/__init__.py
@@ -6,11 +6,28 @@ from tuck import auth
 
 from tuck.db import db_session
 
+from tuck.device_views import DeviceClassView, DeviceView
+
 app = Flask(__name__, instance_relative_config=True)
 
 app.secret_key = getenv("APP_SECRET", "ElsyagfankAmfuffyobshinagCowbij8")
 
 app.register_blueprint(auth.bp)
+
+
+def register_api(view, endpoint, url, pk="id", pk_type="int"):
+    view_func = auth.login_required(view.as_view(endpoint))
+    app.add_url_rule(url, defaults={pk: None}, view_func=view_func, methods=["GET"])
+    app.add_url_rule(url, view_func=view_func, methods=["POST"])
+    app.add_url_rule(
+        "%s<%s:%s>" % (url, pk_type, pk),
+        view_func=view_func,
+        methods=["GET", "PUT", "DELETE"],
+    )
+
+
+register_api(DeviceClassView, "device_class_api", "/api/device_class/", pk="id")
+register_api(DeviceView, "device_api", "/api/device/", pk="id")
 
 
 @app.teardown_appcontext
